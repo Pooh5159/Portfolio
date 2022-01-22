2a1a2e91fa685e78b348b8f7d8745628942e3d16
Pooh5159 <Pierson.Pauline@gmail.com>
2022-01-22 17:20:26 +0100

    .

--- Pages/app.js
+++ Pages/app.js
@@ -6,33 +6,33 @@ let slides = ''
 let m = 0;
 
 function plusSlides(id, n) {
-  var slides = document.querySelectorAll(`#${id} .mySlides`);
-  var dot = document.querySelectorAll(`#${id} .dot`);
-  if (m + n == slides.length) {
-    n = 0 - m
-  }
-  if (m + n == -1) {
-    n = slides.length - 1
-  }
-  slides[m + n].style.display = "block";
-  slides[m].style.display = "none";
-dot[m+n].classList.add("active");
-dot[m].classList.remove("active");
-  m = m + n
+    var slides = document.querySelectorAll(`#${id} .mySlides`);
+    var dot = document.querySelectorAll(`#${id} .dot`);
+    if (m + n == slides.length) {
+        n = 0 - m
+    }
+    if (m + n == -1) {
+        n = slides.length - 1
+    }
+    slides[m + n].style.display = "block";
+    slides[m].style.display = "none";
+    dot[m + n].classList.add("active");
+    dot[m].classList.remove("active");
+    m = m + n
 }
 
 function currentSlide(id, n) {
-  var dot = document.querySelectorAll(`#${id} .dot`);
-  var slides = document.querySelectorAll(`#${id} .mySlides`);
-  for (let i = 0; i < dot.length; i++) {
-    dot[i].classList.remove("active");
-  }
-  for (let i = 0; i < slides.length; i++) {
-    console.log(slides[i]);
-    slides[i].style.display = "none";
-  }
-  dot[n].classList.add("active");
-  slides[n].style.display = "block";
+    var dot = document.querySelectorAll(`#${id} .dot`);
+    var slides = document.querySelectorAll(`#${id} .mySlides`);
+    for (let i = 0; i < dot.length; i++) {
+        dot[i].classList.remove("active");
+    }
+    for (let i = 0; i < slides.length; i++) {
+        console.log(slides[i]);
+        slides[i].style.display = "none";
+    }
+    dot[n].classList.add("active");
+    slides[n].style.display = "block";
 }
 
 
@@ -44,13 +44,19 @@ var img = document.getElementById("#myImg");
 var modalImg = document.getElementById("img");
 
 var video = document.getElementById("#myVid");
-var modalImg = document.getElementById("video");
+var modalVideo = document.getElementById("video");
 
 var captionText = document.getElementById("caption");
 
 function clickNext(id) {
     modal.style.display = "block";
-    modalImg.src = document.getElementById(id).src;
+    if (document.getElementById(id) === img) {
+        modalImg.src = document.getElementById(id).src
+        console.log('image');
+    }
+    else (document.getElementById(id) === video) 
+        modalVideo.src = document.getElementById(id).src
+        console.log('video');
     captionText.innerHTML = document.getElementById(id).alt;
 }
 
